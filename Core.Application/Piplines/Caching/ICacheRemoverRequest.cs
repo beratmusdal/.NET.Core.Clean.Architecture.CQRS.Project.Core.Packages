fe46dabebf20fcbb0a2366782025e87da2a3bc3b
Berat <Berat@DESKTOP-P9FOPDG>
2024-08-25 21:16:40 +0300

    last

--- Core.Application/Piplines/Caching/ICacheRemoverRequest.cs
+++ Core.Application/Piplines/Caching/ICacheRemoverRequest.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace Core.Application.Piplines.Caching;
 
-public interface ICachableRequest
+public interface ICacheRemoverRequest
 {
     string CacheKey { get; }
     bool BypassCache { get; }
     string? CacheGroupKey { get; }
-    TimeSpan? SlidingExpiration { get; }
 }
